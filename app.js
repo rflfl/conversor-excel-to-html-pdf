a96e95daf054d3091019a21d711f2adfa22d0a1b
Rafael Ferreira Lemes <40215566+rflfl@users.noreply.github.com>
2023-10-10 13:52:20 +0000

    Feat: class Table

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const Reader = require('./Reader')
 const Processor = require('./Processor')
+const Table = require('./Table')
 
 let leitor = new Reader()
 
 async function main(){
     let dados = await leitor.Reade('./users.csv')
     let dadosProcessados = Processor.Process(dados)
+
+    let usuarios = new Table(dadosProcessados)
+    console.log(usuarios.header)
+
 }
 
 main()
