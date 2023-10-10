b2314b3e2060aaeb5a6aef5d59142a864be9e9d1
Rafael Ferreira Lemes <40215566+rflfl@users.noreply.github.com>
2023-10-10 14:28:57 +0000

    feat: HtmlParser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Reader = require('./Reader')
 const Processor = require('./Processor')
 const Table = require('./Table')
+const HtmlParser = require('./HtmlParser')
 
 let leitor = new Reader()
 
@@ -9,8 +10,9 @@ async function main(){
     let dadosProcessados = Processor.Process(dados)
 
     let usuarios = new Table(dadosProcessados)
-    console.log(usuarios.RowCount)
-    console.log(usuarios.ColumnCount)
+   
+    let html = await HtmlParser.Parse(usuarios)
+
 
 }
 
